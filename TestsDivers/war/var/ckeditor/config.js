6f5154fef9506f52908f514f90d6ae6e3e778ec3
dsportes <daniel.sportes@laposte.net>
2016-08-22 15:53:03 +0200

    CKEditor

---
+++ TestsDivers/war/var/ckeditor/config.js
@@ -0,0 +1,20 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Sourcedialog,Anchor,Styles';
+};
--- TestsDivers/war/var/main.js
+++ TestsDivers/war/var/main.js
@@ -31,20 +31,45 @@ APP.SubServer = class SubServer extends APP.Server {
 	}
 }
 
-console.log("SS name " + APP.SubServer.name);
-const hw = document.getElementById("hw");
-const hw2 = document.getElementById("hw2");
-hw.addEventListener("click", () => {
-		const srv = new APP.Server("titi");
-		if (srv instanceof APP.Server)
-			console.log("srv hérite de Server");
-		hw.innerHTML = srv.print();
-		// const srv2 = Object.assign(new APP.Server(""), {url:"tutu"});
-		const srv2 = Object.assign(new APP.SubServer(""), {url:"tutu"});
-		if (srv2 instanceof APP.Server)
-			console.log("srv2 hérite de SubServer");
-		if (srv2 instanceof APP.SubServer)
-			console.log("srv2 hérite de Server");
-		hw2.innerHTML = srv2.print();
-	}
-);
+CKEDITOR.disableAutoInline = true;
+
+APP.onload = function() {
+	console.log("SS name " + APP.SubServer.name);
+	const hw = document.getElementById("hw");
+	const hw2 = document.getElementById("hw2");
+	APP.ed1 = document.getElementById("editor1");
+	APP.ed2 = document.getElementById("editor2");
+	
+	CKEDITOR.disableAutoInline = true;
+	CKEDITOR.inline(APP.ed1, {language: 'en'} ).on('change', (evt) => {
+		    // getData() returns CKEditor's HTML content.
+		    console.log( 'Total bytes: ' + evt.editor.getData().length );
+		});
+
+	CKEDITOR.inline(APP.ed2, {language: 'en'} ).on('change', (evt) => {
+		    // getData() returns CKEditor's HTML content.
+		    console.log( 'Total bytes: ' + evt.editor.getData().length );
+		});
+
+	
+	hw.addEventListener("click", () => {
+			const srv = new APP.Server("titi");
+			if (srv instanceof APP.Server)
+				console.log("srv hérite de Server");
+			hw.innerHTML = srv.print();
+			// const srv2 = Object.assign(new APP.Server(""), {url:"tutu"});
+			const srv2 = Object.assign(new APP.SubServer(""), {url:"tutu"});
+			if (srv2 instanceof APP.Server)
+				console.log("srv2 hérite de SubServer");
+			if (srv2 instanceof APP.SubServer)
+				console.log("srv2 hérite de Server");
+			const m = srv2.print();
+			hw2.innerHTML = m;
+			ed1.innerHTML = APP.data;
+		}
+	);
+	hw2.addEventListener("click", () => {
+		APP.data = CKEDITOR.instances.editor1.getData();
+		console.log(data);
+	});
+}
